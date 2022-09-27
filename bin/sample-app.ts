1503d0881c2860ec3f7278cbfc74e45e97dad7d2
KVS SIVA KUMAR <sivakumar.k@comakit.com>
2022-09-27 11:39:56 +0530

    Adding Files:First Commit

--- bin/sample-app.ts
+++ bin/sample-app.ts
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
+// import 'source-map-support/register';
+// import * as cdk from 'aws-cdk-lib';
+// import { SampleAppStack } from '../lib/sample-app-stack';
+
+// const app = new cdk.App();
+// new SampleAppStack(sample-app, 'SampleAppStack', {
+
+// });
+
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { SampleAppStack } from '../lib/sample-app-stack';
+// import { HttpApiStack } from '../lib/httpApi-stack';
+
+const envUSA = { region: 'us-west-2' };
 
 const app = new cdk.App();
-new SampleAppStack(app, 'SampleAppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const fargateVpclinkStack = new SampleAppStack(app, 'SampleAppStack', { env: envUSA });
+// new HttpApiStack(app, 'HttpApiStack', fargateVpclinkStack.httpVpcLink, fargateVpclinkStack.httpApiListener , { env: envUSA });
